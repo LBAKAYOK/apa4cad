2bda3c97a4d9167c990e11031d3c77c6a20370e0
LBAKAYOK <bakayokolassina27@gmail.com>
2026-06-25 10:54:41 +0200

    Fix include config_fuseki dans admin

--- admin/_guard.php
+++ admin/_guard.php
@@ -12,6 +12,7 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/admin_config.php';
+require_once __DIR__ . '/../config_fuseki.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
