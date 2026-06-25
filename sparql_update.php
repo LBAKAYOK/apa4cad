c1e4beba60e08ebaaece6b79d0fdc5c1839927ff
LBAKAYOK <bakayokolassina27@gmail.com>
2026-06-25 11:15:13 +0200

    Retire user:pass de URL endpoint, garde header auth

--- sparql_update.php
+++ sparql_update.php
@@ -18,7 +18,7 @@ require_once __DIR__ . '/config_fuseki.php';
  * @return array ['success' => bool, 'http_code' => int, 'response' => string, 'error' => string|null]
  */
 function sparqlUpdate($updateQuery) {
-    $endpoint = str_replace('http://', 'https://', FUSEKI_UPDATE_ENDPOINT);
+    $endpoint = preg_replace('#https?://[^@]+@#', 'https://', FUSEKI_UPDATE_ENDPOINT);
 
     $ch = curl_init($endpoint);
     curl_setopt($ch, CURLOPT_POST, true);
